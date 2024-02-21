aa1626b3b241f9d2f4d0945cfc49486de92f0007
bushkovsky <62966446+bushkovsky@users.noreply.github.com>
2024-02-21 18:43:33 +0300

    feature rolls dices

--- Dice Sochi/ViewController.swift
+++ Dice Sochi/ViewController.swift
@@ -8,10 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet weak var diceImageOne: UIImageView!
+    
+    @IBOutlet weak var diceImageTwo: UIImageView!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    @IBAction func logoButtonPressed(_ sender: UIButton) {
+        
+        if(button.currentImage == #imageLiteral(resourceName: "AppBreweryLogo")){
+            
+            button.setImage(#imageLiteral(resourceName: "DiceeLogo"), for: UIControl.State.normal)
+            
+        }
+        else{
+            button.setImage(#imageLiteral(resourceName: "AppBreweryLogo"), for: UIControl.State.normal)
+        }
+    }
+    
+    @IBAction func changeDicesImage(_ sender: UIButton) {
+        
+        let dices = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+        diceImageOne.image = dices[Int.random(in: 0...5)]
+        diceImageTwo.image = dices[Int.random(in: 0...5)]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
